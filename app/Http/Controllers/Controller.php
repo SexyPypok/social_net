4ebcd681e34b302935f4032d12542994675cfbcd
mta2003@bk.ru <mta2003@bk.ru>
2021-06-04 14:01:48 +0400

    added comments table

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,20 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+use App\Models\Comments;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function get_comm()
+    {
+        $model = new Comments;
+        $comments = $model::all();
+
+        foreach($comments as $comment)
+        {
+            echo $comment->text;
+        }
+    }
 }
