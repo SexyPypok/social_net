83a1a445927437a65e7960b63b76ad7aefd55033
mta2003@bk.ru <mta2003@bk.ru>
2021-07-01 16:53:59 +0400

    deleted some functions

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -13,22 +13,6 @@ use App\Models\User;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    public function get_comm()
-    {
-        $model = new User;
-        $users = $model->all();
-        foreach($users as $user)
-        {
-            print_r($user['name']);
-            $comments = $user->comments()->get();
-
-            foreach($comments as $comment)
-            {
-                print_r($comment['text']);
-            }
-        }
-    }
     
     public function get_user_id()
     {
